7e481c409007672051531ae353bc55040cb7c195
stanaka2 <stanaka2@student.42tokyo.jp>
2026-06-19 16:24:20 +0900

    fix: vup

--- src/scene/utils/calc_camera_right.c
+++ src/scene/utils/calc_camera_right.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 20:21:43 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/06/19 15:49:33 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/06/19 16:23:04 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ t_dvec3	calc_camera_right(t_dvec3 dir)
 	if (fabs(dir.z) <= 1.0 - 1e-6)
 		vup = (t_dvec3){0.0, 0.0, 1.0};
 	else
-		vup = (t_dvec3){0.0, -1.0, 0.0};
+		vup = (t_dvec3){0.0, 1.0, 0.0};
 	return (dvec3_normalize(dvec3_cross(dir, vup)));
 }
