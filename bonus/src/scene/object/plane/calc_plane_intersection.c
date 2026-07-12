4b38fcd74fff68a5c5a81fa3917ab2e1b67d7827
stanaka2 <stanaka2@student.42tokyo.jp>
2026-07-12 23:44:17 +0900

    fix: from_camera->to_center

--- bonus/src/scene/object/plane/calc_plane_intersection.c
+++ bonus/src/scene/object/plane/calc_plane_intersection.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 00:48:01 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/12 23:06:15 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/12 23:43:03 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 float	calc_plane_intersection(t_plane const *plane, t_ray const *ray)
 {
 	float	dot;
-	t_vec3	from_camera;
+	t_vec3	to_center;
 	float	t;
 
 	dot = vec3_dot(plane->normal, ray->dir);
 	if (fabsf(dot) < EPSILON)
 		return (NAN);
-	from_camera = vec3_sub(ray->origin, plane->center);
-	t = -vec3_dot(plane->normal, from_camera) / dot;
+	to_center = vec3_sub(plane->center, ray->origin);
+	t = vec3_dot(plane->normal,	to_center) / dot;
 	if (t >= 0.0f)
 		return (t);
 	return (NAN);
