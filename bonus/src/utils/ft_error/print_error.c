fe0607235906a9a0f6b532cf1bc6619665cbaa7b
stanaka2 <stanaka2@student.42tokyo.jp>
2026-07-28 19:51:49 +0900

    fix: print_texture_error

--- bonus/src/utils/ft_error/print_error.c
+++ bonus/src/utils/ft_error/print_error.c
@@ -6,11 +6,12 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:20:43 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/28 19:48:28 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/28 19:51:19 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stddef.h>
 
 #include "ft_stdio.h"
 
@@ -23,7 +24,7 @@ void	print_error(char const *msg)
 void	print_texture_error(char const *path, char const *msg)
 {
 	ft_dprintf(STDERR_FILENO, "Error\n");
-	if (*path != '\0')
+	if (path != NULL && *path != '\0')
 		ft_dprintf(STDERR_FILENO, "%s\n", path);
 	ft_dprintf(STDERR_FILENO, "%s\n", msg);
 }
