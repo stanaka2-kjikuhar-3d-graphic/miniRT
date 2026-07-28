093175be1536f780513d2b954af90dfbf0e68bd5
stanaka2 <stanaka2@student.42tokyo.jp>
2026-07-28 19:49:14 +0900

    fix: texture=null

--- bonus/src/utils/ft_error/print_error.c
+++ bonus/src/utils/ft_error/print_error.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:20:43 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/28 18:26:38 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/28 19:48:28 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	print_error(char const *msg)
 void	print_texture_error(char const *path, char const *msg)
 {
 	ft_dprintf(STDERR_FILENO, "Error\n");
-	ft_dprintf(STDERR_FILENO, "%s\n", path);
+	if (*path != '\0')
+		ft_dprintf(STDERR_FILENO, "%s\n", path);
 	ft_dprintf(STDERR_FILENO, "%s\n", msg);
 }
