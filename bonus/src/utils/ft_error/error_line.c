949d1066c95e7bdb5981dfbed6c51abea2be47a2
stanaka2 <stanaka2@student.42tokyo.jp>
2026-07-31 15:12:13 +0900

    renew: error_line

--- bonus/src/utils/ft_error/error_line.c
+++ bonus/src/utils/ft_error/error_line.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 18:01:40 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/31 03:03:16 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/31 14:47:24 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,13 @@
 
 #include "ft_string.h"
 
-#define ERROR_LINE_CAPACITY 1024
-
-static char		g_error_line[ERROR_LINE_CAPACITY];
+static char		*g_error_line;
 static size_t	g_error_line_number;
 
 void	set_error_line(size_t line_number, char const *line)
 {
-	size_t	len;
-
 	g_error_line_number = line_number;
-	len = ft_strlcpy(g_error_line, line, ERROR_LINE_CAPACITY);
-	if (len >= ERROR_LINE_CAPACITY)
-		ft_strlcpy(g_error_line + ERROR_LINE_CAPACITY - 4, "...", 4);
+	g_error_line = line;
 }
 
 size_t	get_error_line_number(void)
