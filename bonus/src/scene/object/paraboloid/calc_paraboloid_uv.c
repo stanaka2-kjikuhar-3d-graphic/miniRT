c8fc79bfd71c8103484d8900133e892b4dd545b6
jiku0730 <github@jiku.jp>
2026-07-31 21:02:31 +0900

    add: calc_paraboloid_uv を実装した。

--- bonus/src/scene/object/paraboloid/calc_paraboloid_uv.c
+++ bonus/src/scene/object/paraboloid/calc_paraboloid_uv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calc_paraboloid_uv.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/05 21:37:42 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/22 21:58:18 by stanaka2         ###   ########.fr       */
+/*   Created: 2026/07/05 00:55:13 by stanaka2          #+#    #+#             */
+/*   Updated: 2026/07/31 21:01:49 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,21 @@
 #include "vector.h"
 #include "object.h"
 
-// TODO: implement paraboloid uv
 t_vec2	calc_paraboloid_uv(t_paraboloid const *paraboloid, t_vec3 point)
 {
-	(void)paraboloid;
-	(void)point;
-	return ((t_vec2){.u = 0.0f, .v = 0.0f});
+	t_vec2	uv;
+	t_vec3	to_point;
+	float	h;
+	t_vec3	radial;
+	float	phi;
+
+	to_point = vec3_sub(point, paraboloid->center);
+	h = vec3_dot(to_point, paraboloid->dir);
+	radial = vec3_normalize(\
+			vec3_sub(to_point, vec3_scale(h, paraboloid->dir)));
+	phi = atan2f(vec3_dot(radial, paraboloid->onb.v), \
+					vec3_dot(radial, paraboloid->onb.u));
+	uv.u = (float)((phi + M_PI) / (2.0f * M_PI));
+	uv.v = h / paraboloid->height;
+	return (uv);
 }
