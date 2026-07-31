923e3450f8ae20cca4baa646931e9b6829bac4f3
jiku0730 <github@jiku.jp>
2026-07-31 18:44:12 +0900

    add: calc_cone_uv を実装した。

--- bonus/src/scene/object/cone/calc_cone_uv.c
+++ bonus/src/scene/object/cone/calc_cone_uv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calc_cone_uv.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 21:37:42 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/22 22:03:27 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/31 18:41:00 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,20 @@
 #include "vector.h"
 #include "object.h"
 
-// TODO: implement cone uv
 t_vec2	calc_cone_uv(t_cone const *cone, t_vec3 point)
 {
-	(void)cone;
-	(void)point;
-	return ((t_vec2){.u = 0.0f, .v = 0.0f});
+	t_vec2	uv;
+	t_vec3	to_point;
+	float	h;
+	t_vec3	radial;
+	float	phi;
+
+	to_point = vec3_sub(point, cone->center);
+	h = vec3_dot(to_point, cone->dir);
+	radial = vec3_normalize(vec3_sub(to_point, vec3_scale(h, cone->dir)));
+	phi = atan2f(vec3_dot(radial, cone->onb.v), \
+					vec3_dot(radial, cone->onb.u));
+	uv.u = (float)((phi + M_PI) / (2.0f * M_PI));
+	uv.v = h / cone->height;
+	return (uv);
 }
