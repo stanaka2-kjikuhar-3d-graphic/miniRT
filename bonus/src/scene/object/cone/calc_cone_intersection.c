343e893c61b22c756bd34b11dfb40718f104e16a
jiku0730 <github@jiku.jp>
2026-07-31 18:40:07 +0900

    add: calc_cone_intersection / calc_cone_normal を実装した。

--- bonus/src/scene/object/cone/calc_cone_intersection.c
+++ bonus/src/scene/object/cone/calc_cone_intersection.c
@@ -3,26 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   calc_cone_intersection.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 02:41:38 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/22 22:03:13 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/07/31 18:39:00 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <math.h>
-#include <stdbool.h>
-
-#include "vector.h"
 #include "object.h"
 #include "ray.h"
 
 #include "../object_private.h"
 
-// TODO: implement cone intersection
 float	calc_cone_intersection(t_cone const *cone, t_ray const *ray)
 {
-	(void)cone;
-	(void)ray;
-	return (NAN);
+	t_quadric	quad;
+
+	cone_to_quadric(cone, &quad);
+	return (calc_quadric_intersection(&quad, ray));
 }
