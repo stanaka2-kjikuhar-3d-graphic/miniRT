ae836d3a86b402a4c829d75a33f57f6322bff46c
jiku0730 <github@jiku.jp>
2026-08-01 16:31:10 +0900

    refactor: calc_sphere_tbnをvec3_divで書き換えた。

--- bonus/src/scene/object/sphere/calc_sphere_tbn.c
+++ bonus/src/scene/object/sphere/calc_sphere_tbn.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 19:54:35 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/22 00:39:45 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/08/01 16:29:50 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_onb	calc_sphere_tbn(t_sphere const *sphere, t_vec3 normal)
 	len = sqrtf(tu * tu + tv * tv);
 	if (len < EPSILON)
 		return (sphere->onb);
-	tbn.u = vec3_scale(1.0f / len, vec3_sub(\
+	tbn.u = vec3_div(len, vec3_sub(\
 				vec3_scale(tu, sphere->onb.v), vec3_scale(tv, sphere->onb.u)));
 	tbn.v = vec3_cross(tbn.u, tbn.w);
 	return (tbn);
