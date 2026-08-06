c69d57101ab52b36dc1c332cda380de9cbfa8f29
jiku0730 <github@jiku.jp>
2026-08-06 21:48:00 +0900

    refactor: hyperboloidで毎回Qを構築するのをやめてCacheを参照するようにした。

--- bonus/src/scene/object/hyperboloid/calc_hyperboloid_intersection.c
+++ bonus/src/scene/object/hyperboloid/calc_hyperboloid_intersection.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 06:40:18 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/31 21:30:00 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/08/06 21:33:31 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,5 @@
 float	calc_hyperboloid_intersection(\
 	t_hyperboloid const *hyperboloid, t_ray const *ray)
 {
-	t_quadric	quad;
-
-	hyperboloid_to_quadric(hyperboloid, &quad);
-	return (calc_quadric_intersection(&quad, ray));
+	return (calc_quadric_intersection(&(hyperboloid->quadric), ray));
 }
