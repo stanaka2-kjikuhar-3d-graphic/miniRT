0cf80e75b949b1a7974f0d8ac167aa813bf19359
jiku0730 <github@jiku.jp>
2026-08-07 01:08:56 +0900

    add: local->world 行列を組む mat4_local_to_world を追加した。

--- Makefile
+++ Makefile
@@ -399,6 +399,7 @@ SRCS	+=	mat4_identity.c \
 			mat4_scale.c \
 			mat4_rotate.c \
 			mat4_basis.c \
+			mat4_local_to_world.c \
 			mat4_inverse.c
 
 # utils/matrix/internal
--- bonus/include/utils/matrix.h
+++ bonus/include/utils/matrix.h
@@ -6,15 +6,30 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 21:43:59 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/07/22 22:19:19 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/08/07 01:04:59 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATRIX_H
 # define MATRIX_H
 
+# include <stdbool.h>
+
 # include "vector.h"
 
+/*
+t_mat3: basis vectors are COLUMNS.
+
+              X_AXIS   Y_AXIS   Z_AXIS
+           +----------------------------+
+    row 0  |   u.x      v.x      w.x    |
+    row 1  |   u.y      v.y      w.y    |
+    row 2  |   u.z      v.z      w.z    |
+           +----------------------------+
+
+  local -> world :  M * p
+  world -> local :  transpose(M) * p   (orthonormal basis only)
+*/
 typedef struct s_mat3
 {
 	float	m[3][3];
@@ -37,6 +52,8 @@ t_mat4	mat4_translate(t_vec3 t);
 t_mat4	mat4_scale(t_vec3 s);
 t_mat4	mat4_rotate(t_vec3 axis, float degree);
 t_mat4	mat4_basis(t_mat3 const *basis, t_vec3 origin);
+t_mat4	mat4_local_to_world(\
+			t_mat3 const *basis, t_vec3 origin, t_vec3 scale);
 
 t_mat3	mat3_from_mat4(t_mat4 const *m);
 
