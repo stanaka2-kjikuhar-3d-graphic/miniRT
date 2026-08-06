2db03bf0a850acacfb498abf56ecaabd24c8d530
jiku0730 <github@jiku.jp>
2026-08-06 19:51:35 +0900

    refactor: add_color の入れ子をやめて逐次加算にした。

--- bonus/src/renderer/phong/lighting/phong_lighting_point.c
+++ bonus/src/renderer/phong/lighting/phong_lighting_point.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phong_lighting_point.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 03:38:56 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/08/01 16:15:20 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/08/06 19:35:11 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,9 @@ void	phong_lighting_point(t_color *color, \
 	if (!phong_shading(hit, light_dir, light_dist))
 	{
 		attenuation = calc_point_light_attenuation(light, light_dist);
-		*color = add_color(add_color(\
-					*color, \
-					calc_diffuse_color(hit, light, attenuation)), \
-					calc_specular_color(ray, hit, light, attenuation) \
-				);
+		*color = add_color(*color, calc_diffuse_color(hit, light, attenuation));
+		*color = add_color(*color, \
+			calc_specular_color(ray, hit, light, attenuation));
 	}
 }
 
