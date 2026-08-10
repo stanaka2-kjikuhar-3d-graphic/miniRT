bae94e700df242b1827b2197bb7964ad5a30a26a
stanaka2 <stanaka2@student.42tokyo.jp>
2026-08-11 02:31:26 +0900

    add: calc_aabb_intersection

---
+++ bonus/src/scene/object/calc_aabb_intersection.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc_aabb_intersection.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 00:58:45 by stanaka2          #+#    #+#             */
+/*   Updated: 2026/08/11 02:27:24 by stanaka2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <math.h>
+#include <stdbool.h>
+
+#include "object.h"
+#include "ray.h"
+#include "vector.h"
+
+static t_range	calc_axis_range(float origin, float inv_dir, t_range axis);
+
+float	calc_aabb_intersection(\
+	t_vec3 ray_origin, t_vec3 ray_inv_dir, t_aabb const *aabb)
+{
+	t_range	axis_range[3];
+	t_range	t;
+
+	axis_range[X_AXIS] = calc_axis_range(ray_origin.x, ray_inv_dir.x, aabb->x);
+	axis_range[Y_AXIS] = calc_axis_range(ray_origin.y, ray_inv_dir.y, aabb->y);
+	axis_range[Z_AXIS] = calc_axis_range(ray_origin.z, ray_inv_dir.z, aabb->z);
+	t.min = fmaxf(fmaxf(fmaxf(axis_range[X_AXIS].min, axis_range[Y_AXIS].min), \
+									axis_range[Z_AXIS].min), 0.0f);
+	t.max = fminf(fminf(axis_range[X_AXIS].max, axis_range[Y_AXIS].max), \
+							axis_range[Z_AXIS].max);
+	if (t.max < t.min)
+		return (NAN);
+	return (t.min);
+}
+
+static t_range	calc_axis_range(float origin, float inv_dir, t_range axis)
+{
+	float	t0;
+	float	t1;
+
+	if (isinf(inv_dir))
+	{
+		if (origin < axis.min || axis.max < origin)
+			return ((t_range){.min = 0.0f, .max = -INFINITY});
+		return ((t_range){.min = 0.0f, .max = INFINITY});
+	}
+	t0 = (axis.min - origin) * inv_dir;
+	t1 = (axis.max - origin) * inv_dir;
+	return ((t_range){.min = fminf(t0, t1), .max = fmaxf(t0, t1)});
+}
