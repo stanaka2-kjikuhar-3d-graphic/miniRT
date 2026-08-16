bdee2ca9d38c885e68f9764aab6dcb868bc27c16
jiku0730 <github@jiku.jp>
2026-08-16 17:22:58 +0900

    fix: ローカルのレイ方向を単位長にして形状の大きさへの依存をなくした。

--- bonus/src/scene/object/primitive/calc_primitive_intersection.c
+++ bonus/src/scene/object/primitive/calc_primitive_intersection.c
@@ -6,10 +6,13 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 02:04:11 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/08/16 16:53:12 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/08/16 17:21:36 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
+
+#include "config.h"
 #include "matrix.h"
 #include "object.h"
 #include "ray.h"
@@ -23,11 +26,16 @@ float	calc_primitive_intersection(\
 	t_primitive const *prim, t_ray const *ray)
 {
 	t_ray	local;
+	float	len;
 
 	local = ray_to_local(&(prim->to_local), ray);
 	if (prim->type == UNIT_PLANE || prim->type == UNIT_DISC)
 		return (calc_planar_intersection(prim, &local));
-	return (solve_unit_form(prim, &local));
+	len = vec3_length(local.dir);
+	if (len < EPSILON)
+		return (NAN);
+	local.dir = vec3_div(len, local.dir);
+	return (solve_unit_form(prim, &local) / len);
 }
 
 static t_ray	ray_to_local(t_mat4 const *to_local, t_ray const *ray)
