29c2521158330748d7ff276c270b0528c2ebdff2
takau-jp <takausoccer@icloud.com>
2026-08-16 22:37:37 +0900

    renew: bvh_shadowing

--- bonus/src/renderer/phong/shading/bvh_shading.c
+++ bonus/src/renderer/phong/shading/bvh_shading.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 22:20:00 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/08/16 22:18:16 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/08/16 22:35:48 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@
 #include "../phong_private.h"
 
 static bool	shadow_traverse_node(t_ray const *ray, float dist, \
-				t_bvh_node const *node, float t);
-static bool	shadow_traverse_children(\
-				t_ray const *ray, float dist, t_bvh_node const *node);
+				t_bvh_node const *node);
 static bool	shadow_traverse_leaves(t_ray const *ray, float dist, \
 				size_t start, size_t count);
 
@@ -36,34 +34,34 @@ bool	bvh_shading(t_ray const *ray, float dist)
 	if (root == NULL)
 		return (false);
 	t = calc_aabb_intersection(&(root->aabb), ray);
-	return (shadow_traverse_node(ray, dist, root, t));
+	if (isnan(t) || dist <= t)
+		return (false);
+	return (shadow_traverse_node(ray, dist, root));
 }
 
 /* any-hit: stops at the first blocker instead of keeping the nearest one */
-static bool	shadow_traverse_node(t_ray const *ray, float dist, \
-	t_bvh_node const *node, float t)
-{
-	if (isnan(t) || dist <= t)
-		return (false);
-	if (node->has_leaf)
-		return (shadow_traverse_leaves(ray, dist, \
-				node->leaves.start, node->leaves.count));
-	return (shadow_traverse_children(ray, dist, node));
-}
-
-static bool	shadow_traverse_children(\
+static bool	shadow_traverse_node(\
 	t_ray const *ray, float dist, t_bvh_node const *node)
 {
-	t_bvh_node const	*child[2];
-	float				t[2];
+	t_bvh_node const	*left;
+	t_bvh_node const	*right;
+	float				t;
 
-	child[0] = get_bvh_node(node->branch.left);
-	child[1] = get_bvh_node(node->branch.right);
-	t[0] = calc_aabb_intersection(&(child[0]->aabb), ray);
-	t[1] = calc_aabb_intersection(&(child[1]->aabb), ray);
-	if (shadow_traverse_node(ray, dist, child[0], t[0]))
-		return (true);
-	return (shadow_traverse_node(ray, dist, child[1], t[1]));
+	if (node->has_leaf)
+	{
+		return (shadow_traverse_leaves(ray, dist, \
+					node->leaves.start, node->leaves.count));
+	}
+	else
+	{
+		left = get_bvh_node(node->branch.left);
+		t = calc_aabb_intersection(&(left->aabb), ray);
+		if (t < dist && shadow_traverse_node(ray, dist, left))
+			return (true);
+		right = get_bvh_node(node->branch.right);
+		t = calc_aabb_intersection(&(right->aabb), ray);
+		return (t < dist && shadow_traverse_node(ray, dist, right));
+	}
 }
 
 static bool	shadow_traverse_leaves(t_ray const *ray, float dist, \
