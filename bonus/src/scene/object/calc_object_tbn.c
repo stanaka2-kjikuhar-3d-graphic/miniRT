986e83b38453c1a9136823cd4599ef1f48010f64
jiku0730 <github@jiku.jp>
2026-08-16 21:00:48 +0900

    refactor: 接空間の基底を t_onb から t_mat3 に変え、primitive 経路に切り替えた。

--- bonus/src/scene/object/calc_object_tbn.c
+++ bonus/src/scene/object/calc_object_tbn.c
@@ -6,30 +6,18 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 22:51:01 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/07/22 00:54:44 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/08/16 20:58:11 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "matrix.h"
 #include "object.h"
 #include "vector.h"
 
 #include "./object_private.h"
 
-t_onb	calc_object_tbn(t_object const *object, t_vec3 point, t_vec3 normal)
+t_mat3	calc_object_tbn(t_object const *object, t_vec3 point, t_vec3 normal)
 {
-	if (object->type == OBJ_SPHERE)
-		return (calc_sphere_tbn(&(object->sphere), normal));
-	else if (object->type == OBJ_PLANE)
-		return (calc_plane_tbn(&(object->plane), normal));
-	else if (object->type == OBJ_CYLINDER)
-		return (calc_cylinder_tbn(&(object->cylinder), normal));
-	else if (object->type == OBJ_CIRCLE)
-		return (calc_circle_tbn(\
-				&(object->circle), point, normal, object->uv.type));
-	else
-	{
-		return ((t_onb){(t_vec3){1, 0, 0}, \
-						(t_vec3){0, 1, 0}, \
-						(t_vec3){0, 0, 1}});
-	}
+	return (calc_primitive_tbn(&(object->primitive), point, normal, \
+			object->uv.type));
 }
