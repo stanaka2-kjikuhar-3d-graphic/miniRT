49bd8085a88059eaeb3a97b259a05cf4dd31be43
jiku0730 <github@jiku.jp>
2026-08-16 19:37:19 +0900

    add: 正準ローカル座標から UV を求める calc_primitive_uv を追加した。

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/05/14 13:25:37 by kjikuhar          #+#    #+#              #
-#    Updated: 2026/08/16 19:01:52 by kjikuhar         ###   ########.fr        #
+#    Updated: 2026/08/16 19:36:02 by kjikuhar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -397,7 +397,8 @@ SRCS	+=	build_primitive.c \
 			calc_planar_intersection.c \
 			calc_primitive_intersection.c \
 			solve_unit_form.c \
-			calc_primitive_normal.c
+			calc_primitive_normal.c \
+			calc_primitive_uv.c
 
 # utils/matrix
 SRCS	+=	mat4_identity.c \
--- bonus/src/scene/object/object_private.h
+++ bonus/src/scene/object/object_private.h
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 01:32:49 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/08/16 19:01:27 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/08/16 19:35:44 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,8 @@ float	calc_primitive_intersection(\
 float	solve_unit_form(t_primitive const *prim, t_ray const *local);
 t_vec3	calc_primitive_normal(\
 			t_primitive const *prim, t_ray const *ray, t_vec3 point);
+t_vec2	calc_primitive_uv(t_primitive const *prim, t_vec3 point, \
+			enum e_uv_type uv_type);
 float	calc_planar_intersection(\
 			t_primitive const *prim, t_ray const *local);
 t_vec2	adjust_uv_range(t_vec2 uv, t_range u_range, t_range v_range);
