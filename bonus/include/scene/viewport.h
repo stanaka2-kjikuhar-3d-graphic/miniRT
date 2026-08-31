356af33dd942537d249b1c9624f7e1d9d50412a3
takau-jp <takausoccer@icloud.com>
2026-08-31 22:13:58 +0900

    fix: indent

--- bonus/include/scene/viewport.h
+++ bonus/include/scene/viewport.h
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 00:24:54 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/08/07 23:19:18 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/08/31 21:59:48 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 
 typedef struct s_viewport
 {
-	t_ivec2		pixel_size;
-	int			pixel_count;
-	t_vec2		pixel_half_size;
-	float		aspect_ratio;
-	float		fov;
-	t_vec2		world_half_size;
+	t_ivec2	pixel_size;
+	int		pixel_count;
+	t_vec2	pixel_half_size;
+	float	aspect_ratio;
+	float	fov;
+	t_vec2	world_half_size;
 }	t_viewport;
 
 // input
